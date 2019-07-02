2d3be2b0726edcb71d4180d1c0b75155b16fe97c
Пользователь <Sviatoslv>
2019-07-02 22:28:55 +0300

    Updates

---
+++ precache-manifest.e3dd20ad294cf3bdbfa3c713926bd34a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa331778f1226d66e190b120a0a2f91b",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "ff2ddc6ce85ffd62a7c4",
+    "url": "/react_static-list-of-posts/static/css/main.f2f89f98.chunk.css"
+  },
+  {
+    "revision": "54e7b26bd347c3257311",
+    "url": "/react_static-list-of-posts/static/js/2.45cd7ba0.chunk.js"
+  },
+  {
+    "revision": "ff2ddc6ce85ffd62a7c4",
+    "url": "/react_static-list-of-posts/static/js/main.3cd35f13.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
